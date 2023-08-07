11508f542dd1759393a4586840f1be9b18e45654
ProgramKingAbel <abelmorara254@gmail.com>
2023-08-07 17:28:14 +0300

    Define node and Linked List structure

---
+++ Algos-and-DS/LinkedList/singly-linkedlist.js
@@ -0,0 +1,26 @@
+/**
+ * Create a node structure
+ */
+
+class Node {
+	constructor(value, next_node=null)
+	{
+		this.data = value;
+		this.next_node = next_node
+	}
+
+}
+
+/**
+ * Define Linked List structure
+ */
+
+class LinkedList {
+	constructor()
+	{
+		this.head = null;
+		this.tail = null;
+		this.size = 0;
+	}
+	
+}
