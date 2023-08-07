f9765d01f29c40c7a41d4c1749a754d45e138a9d
ProgramKingAbel <abelmorara254@gmail.com>
2023-08-07 18:09:45 +0300

    Implement Get Add and Remove functionality in LinkedList

--- Algos-and-DS/LinkedList/singly-linkedlist.js
+++ Algos-and-DS/LinkedList/singly-linkedlist.js
@@ -22,5 +22,48 @@ class LinkedList {
 		this.tail = null;
 		this.size = 0;
 	}
-	
+
+	add(number) {
+		const newNode = new Node(number);
+		if (!this.size)
+		{
+			this.head = newNode;
+			this.tail = newNode;
+		} else {
+			this.tail.next_node = newNode;
+			this.tail = newNode;
+
+		}
+		this.size++;
+	}
+
+	get(index) {
+		if (index > this.size || index < 0)
+		{
+			return null;
+		}
+		let current = this.head;
+		for (let i = 0; i < index; i++) {
+			current = current.next_node;
+		}
+		return current; // return current != null ? current.value : null;
+	}
+
+	remove (index) {
+		if (index < 0 || index > this.length) {
+			return null;
+		} else if (index == 0)
+		{
+			this.head = this.head.next_node;
+		} else {
+			const prev_node = this.get(index - 1);
+			prev_node.next_node = prev_node.next_node.next_node;
+			if (!prev_node.next_node) {
+
+				this.tail = prev_node;
+			}
+		}
+		this.size--;
+	}
+				
 }
