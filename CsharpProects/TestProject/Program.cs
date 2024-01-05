8f2fd4ef9f35c2df19f5fa8768ffe4a2aa7f05d9
ProgramKingAbel <abelmorara254@gmail.com>
2024-01-05 16:09:42 +0300

    feat: expiry and renewal service contionals

--- CsharpProects/TestProject/Program.cs
+++ CsharpProects/TestProject/Program.cs
@@ -1,54 +1,25 @@
-﻿// Implement stateful/instance methods
+﻿Random random = new();
+int daysUntilExpiration = random.Next(12);
+Console.WriteLine(daysUntilExpiration);
+// int discountPercentafge = 0;
 
-// Random dice = new Random();
-// int roll = dice.Next(1, 7);
-// int roll1 = dice.Next();
-// int roll2 = dice.Next(50, 101);
-// Console.WriteLine($"Roll: {roll}");
-// Console.WriteLine($"Roll1: {roll1}");
-// Console.WriteLine($"Roll2: {roll2}");
 
-// int firstValue = 500;
-// int secondValue = 600;
-// int largerValue = Math.Max(firstValue, secondValue);
-
-// Console.WriteLine(largerValue);
-
-Random dice = new();
-int roll1 = dice.Next(1, 7);
-int roll2 = dice.Next(1, 7);
-int roll3 = dice.Next(1, 7);
-
-int total = roll1 + roll2 + roll3;
-Console.WriteLine($"Dice Roll: {roll1} + {roll2} + {roll3} = {total}");
-
-if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
-{
-    if ((roll1 == roll2) && (roll2 == roll3))
-    {
-        Console.WriteLine("You rolled triples! +6 bonus to total!");
-        total += 6;
-    }
-    else
-    {
-        Console.WriteLine("You rolled doubles! +2 bonus to total!");
-        total += 2;
-    }
+if (daysUntilExpiration == 0) {
+    Console.WriteLine("Your subscription has expired.");
 }
 
-if (total >= 16)
-{
-    Console.WriteLine("You win a new car!");
+else if (daysUntilExpiration == 1) {
+    Console.WriteLine("Your subscription expires within a day! \n Renew now and save 20%!");
 }
-else if (total >= 10)
-{
-    Console.WriteLine("You win a new laptop!");
+
+if (daysUntilExpiration > 1 && daysUntilExpiration <= 5) {
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days. \n Renew now and save 10%!");
 }
-else if (total == 7)
-{
-    Console.WriteLine("You win a trip for two!");
+
+else if (daysUntilExpiration > 5 && daysUntilExpiration <= 10) {
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
 }
-else
-{
-    Console.WriteLine("You win a kitten!");
+
+else {
+    Console.WriteLine(" ");
 }
