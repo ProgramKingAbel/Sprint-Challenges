31b79c3746ae0a17b956e0e89d95dc625c57c66a
ProgramKingAbel <abelmorara254@gmail.com>
2024-01-05 15:10:07 +0300

    feat: Utilize overloaded methods

--- CsharpProects/TestProject/Program.cs
+++ CsharpProects/TestProject/Program.cs
@@ -2,4 +2,14 @@
 
 Random dice = new Random();
 int roll = dice.Next(1, 7);
-Console.WriteLine(roll);
+int roll1 = dice.Next();
+int roll2 = dice.Next(50, 101);
+Console.WriteLine($"Roll: {roll}");
+Console.WriteLine($"Roll1: {roll1}");
+Console.WriteLine($"Roll2: {roll2}");
+
+int firstValue = 500;
+int secondValue = 600;
+int largerValue = Math.Max(firstValue, secondValue);
+
+Console.WriteLine(largerValue);
