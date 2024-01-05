0225b015b57b0696912876bd97c8efd358149a70
ProgramKingAbel <abelmorara254@gmail.com>
2024-01-05 19:32:04 +0300

    feat: code readability best practise

--- CsharpProects/TestProject/Program.cs
+++ CsharpProects/TestProject/Program.cs
@@ -1,13 +1,58 @@
-﻿int[] inventory = { 200, 450, 700, 175, 250 };
+﻿string[] fraudulentOrderIDs = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
 
-int sum = 0;
-int bin = 0;
-
-foreach (int value in inventory) {
-    sum += value;
-    bin++;
-    Console.WriteLine($"Bin {bin} = {value} items (Running total: {sum}) ");
+foreach (string orderId in fraudulentOrderIDs)
+{
+    if (orderId.StartsWith("B"))
+    {
+        Console.WriteLine(orderId);
+    }
 }
 
+/*
+  The following code creates five random OrderIDs
+  to test the fraud detection process.  OrderIDs 
+  consist of a letter from A to E, and a three
+  digit number. Ex. A123.
+*/
+Random random = new Random();
+string[] orderIDs = new string[5];
 
-Console.WriteLine($"We have {sum} number of items in the inventory");
+for (int i = 0; i < orderIDs.Length; i++)
+{
+    int prefixValue = random.Next(65, 70);
+    string prefix = Convert.ToChar(prefixValue).ToString();
+    string suffix = random.Next(1, 1000).ToString("000");
+
+    orderIDs[i] = prefix + suffix;
+}
+
+foreach (var orderID in orderIDs)
+{
+    Console.WriteLine(orderID);
+}
+
+/*
+   This code reverses a message, counts the number of times 
+   a particular character appears, then prints the results
+   to the console window.
+ */
+
+string str = "The quick brown fox jumps over the lazy dog.";
+
+char[] charMessage = str.ToCharArray();
+
+Array.Reverse(charMessage);
+int x = 0;
+
+foreach (char i in charMessage)
+{
+    if (i == 'o')
+    {
+        x++;
+    }
+}
+
+string new_message = new String(charMessage);
+
+Console.WriteLine(new_message);
+Console.WriteLine($"'o' appears {x} times.");
