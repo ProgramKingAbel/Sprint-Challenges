4e3bb494183896b143ba17be7c46f94ef7a712b3
ProgramKingAbel <abelmorara254@gmail.com>
2024-01-05 14:21:05 +0300

    ci(methods): Implement stateful methods

--- CsharpProects/TestProject/Program.cs
+++ CsharpProects/TestProject/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World of C#!");
+﻿// Implement stateful/instance methods
+
+Random dice = new Random();
+int roll = dice.Next(1, 7);
+Console.WriteLine(roll);
